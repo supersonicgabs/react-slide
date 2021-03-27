e61f898e124dbe190436ad37ec68b81aaed759fc
Gabriel Fonseca <gabrielfonseca.coutinho@outlook.com>
2021-03-27 12:31:53 +0000

    native effects

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
-import {useEffect, useState} from "react";
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
-import { SwitchTransition, CSSTransition } from "react-transition-group";
+import Slider from "./components/slider"
 
 
 function App() {
-  const [activeImageIndex, setActiveImageIndex] = useState(0)
-  const images = [
-    {
-      id: 1,
-      name: "A New Hope",
-      url: "https://www.ewbankauctions.co.uk/images/NR011606-1_1.jpg"
-    },
-    {
-      id: 2,
-      name: "Empire Strikes Back",
-      url: "https://www.bobafettfanclub.com/tn/640x640/multimedia/galleries/albums/userpics/10001/the-empire-strikes-back-poster.jpg"
-    },
-    {
-      id: 3,
-      name: "Return Of The Jedi",
-      url: "https://cdn20.pamono.com/p/g/5/9/593964_yaneshw35r/star-wars-return-of-the-jedi-poster-by-josh-kirby-1983-1.jpg"
-    },
-  ]
+  
 
   return (
     <div className="App">
-      <div className="carousel-details">
-        <button type="button" onClick={()=>setActiveImageIndex(activeImageIndex!==0 ? activeImageIndex - 1 : images.length-1)}>
-          <FaAngleLeft />
-        </button>
-        <SwitchTransition mode="out-in">
-          <CSSTransition
-            key={activeImageIndex}
-            addEndListener={(node, done) => {
-              node.addEventListener("transitionend", done, false);
-            }}
-            classNames="fade"
-          >
-            <img src={images[activeImageIndex].url} alt={images[activeImageIndex].name} />
-          </CSSTransition>
-        </SwitchTransition>
-        <button type="button" onClick={()=>setActiveImageIndex(activeImageIndex!==images.length-1 ? activeImageIndex + 1 : 0)}>
-          <FaAngleRight />
-        </button>
-      </div>
+      <Slider mode="slide" />
     </div>
   );
 }
---
+++ src/components/slider.js
@@ -0,0 +1,64 @@
+import {useEffect, useState} from "react";
+import PropTypes from 'prop-types'
+import '../App.css';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
+const Slider = ({mode}) => {
+    const [activeImageIndex, setActiveImageIndex] = useState(0)
+    const [className, setClassName] = useState('')
+    useEffect(()=>{
+  
+    }, [activeImageIndex])
+    const images = [
+      {
+        id: 1,
+        name: "A New Hope",
+        url: "https://www.ewbankauctions.co.uk/images/NR011606-1_1.jpg"
+      },
+      {
+        id: 2,
+        name: "Empire Strikes Back",
+        url: "https://www.bobafettfanclub.com/tn/640x640/multimedia/galleries/albums/userpics/10001/the-empire-strikes-back-poster.jpg"
+      },
+      {
+        id: 3,
+        name: "Return Of The Jedi",
+        url: "https://cdn20.pamono.com/p/g/5/9/593964_yaneshw35r/star-wars-return-of-the-jedi-poster-by-josh-kirby-1983-1.jpg"
+      },
+    ]
+  
+    const prevSlide=()=>{
+      setActiveImageIndex(activeImageIndex!==0 ? activeImageIndex - 1 : images.length-1)
+      setClassName(mode === 'slide' ? 'animation-slide-left' : 'animation-fade')
+      setTimeout(()=>{
+        setClassName('')
+      }, 600)
+    }
+  
+    const nextSlide=()=>{
+      setActiveImageIndex(activeImageIndex!==images.length-1 ? activeImageIndex + 1 : 0)
+      setClassName(mode === 'slide' ? 'animation-slide-right' : 'animation-fade')
+      setTimeout(()=>{
+        setClassName('')
+      }, 600)    
+    }
+  
+    return (
+      <div className="App">
+        <div className="carousel-details">
+          <button type="button" onClick={prevSlide}>
+            <FaAngleLeft />
+          </button>
+          <img src={images[activeImageIndex].url} alt={images[activeImageIndex].name} className={className} />
+          <button type="button" onClick={nextSlide}>
+            <FaAngleRight />
+          </button>
+        </div>
+      </div>
+    );    
+}
+
+Slider.propTypes = {
+    mode: PropTypes.oneOf(['fade', 'slide'])
+}
+export default Slider
