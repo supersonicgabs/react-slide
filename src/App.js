9c8ed9cda240d06166d81f069712970290c9ce4c
Gabriel Fonseca <gabrielfonseca.coutinho@outlook.com>
2021-04-03 11:02:48 +0100

    improving the component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import './App.css';
 import Slider from "./components/slider.jsx"
 
 function App() {
-  
+  const images = [
+    {
+      id: 1,
+      name: "A New Hope",
+      url: "https://www.ewbankauctions.co.uk/images/NR011606-1_1.jpg"
+    },
+    {
+      id: 2,
+      name: "Empire Strikes Back",
+      url: "https://www.bobafettfanclub.com/tn/640x640/multimedia/galleries/albums/userpics/10001/the-empire-strikes-back-poster.jpg"
+    },
+    {
+      id: 3,
+      name: "Return Of The Jedi",
+      url: "https://cdn20.pamono.com/p/g/5/9/593964_yaneshw35r/star-wars-return-of-the-jedi-poster-by-josh-kirby-1983-1.jpg"
+    },
+  ]
+   
   return (
     <div className="App">
-      <Slider mode="slide" />
+      <Slider mode="slide" images={images} />
     </div>
   );
 }
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -3,27 +3,9 @@ import PropTypes from 'prop-types'
 import '../App.css';
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
-const Slider = ({mode}) => {
+const Slider = ({mode, images}) => {
     const [activeImageIndex, setActiveImageIndex] = useState(0)
     const [className, setClassName] = useState('')
-
-    const images = [
-      {
-        id: 1,
-        name: "A New Hope",
-        url: "https://www.ewbankauctions.co.uk/images/NR011606-1_1.jpg"
-      },
-      {
-        id: 2,
-        name: "Empire Strikes Back",
-        url: "https://www.bobafettfanclub.com/tn/640x640/multimedia/galleries/albums/userpics/10001/the-empire-strikes-back-poster.jpg"
-      },
-      {
-        id: 3,
-        name: "Return Of The Jedi",
-        url: "https://cdn20.pamono.com/p/g/5/9/593964_yaneshw35r/star-wars-return-of-the-jedi-poster-by-josh-kirby-1983-1.jpg"
-      },
-    ]
   
     const prevSlide=()=>{
       setActiveImageIndex(activeImageIndex!==0 ? activeImageIndex - 1 : images.length-1)
@@ -57,6 +39,7 @@ const Slider = ({mode}) => {
 }
 
 Slider.propTypes = {
-    mode: PropTypes.oneOf(['fade', 'slide'])
+    mode: PropTypes.oneOf(['fade', 'slide']),
+    images: PropTypes.array
 }
 export default Slider
